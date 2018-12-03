e2c3cc4dc6ae124ae6dc9e739ad1f99cb50df5ad
Eric Willroth <ewillroth@gmail.com>
2018-12-03 17:45:46 -0600

    make changes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Student from "./components/Student";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Student from './components/Student';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      money: []
-    };
-  }
-  render() {
-    console.log(this.props);
+	constructor(props) {
+		super(props);
+		this.state = {
+			money: 0
+		};
+		this.giveMoney = this.giveMoney.bind(this);
+		this.spendMoney = this.spendMoney.bind(this);
+	}
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>One-Way Data Binding</h1>
-        </header>
+	giveMoney() {
+		this.setState({
+			money: this.state.money + 5
+		});
+	}
 
-        <Student
-          age={4}
-          name="Taneesha"
-          from="Houston, TX"
-          stickers={["Muy bien", "Especial"]}
-          marker={{
-            color: "Black",
-            brand: "Expo"
-          }}
-          money={this.state.money}
-          spendMoney={() => console.log("spent money")}
-        />
-        <p>
-          {this.state.money[0]
-            ? this.state.money[0].denomination
-            : "No money :("}
-        </p>
-        <button
-          onClick={() => {
-            const newMoney = [
-              ...this.state.money,
-              { denomination: 5, currency: "Chinese Yuan" }
-            ];
-            this.setState({ money: newMoney });
-          }}
-        >
-          Give me money
-        </button>
-      </div>
-    );
-  }
+	spendMoney() {
+		this.setState({
+			money: this.state.money - 5
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1>One-Way Data Binding</h1>
+				</header>
+
+				<Student
+					name="Braden"
+					money={this.state.money}
+					giveMoney={this.giveMoney}
+					spendMoney={this.spendMoney}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,36 +1,19 @@
-import React from "react";
-import AddItem from "./AddItem";
+import React from 'react';
+import AddItem from './AddItem';
 
 const Student = props => {
-  console.log(props);
-  return (
-    <div>
-      <h2>Name: {props.name}</h2>
-      <h3>From: {props.from} </h3>
-      <h3>Age: {props.age}</h3>
-      <div>
-        <h4>Items in my props back pack:</h4>
-        <ul>
-          <li>
-            Money:{" "}
-            {props.money.map(bill => (
-              <strong>
-                {bill.denomination} {bill.currency},{" "}
-              </strong>
-            ))}
-          </li>
-          <li>
-            Stickers:{" "}
-            {props.stickers.map(sticker => (
-              <strong>{sticker + ", "}</strong>
-            ))}
-          </li>
-        </ul>
-        <button onClick={props.spendMoney}>Spend Money</button>
-      </div>
-      <AddItem />
-    </div>
-  );
+	console.log(props);
+	return (
+		<div>
+			<h2>Name: {props.name}</h2>
+			<div>
+				<h4>Money:{props.money}</h4>
+				<button onClick={props.spendMoney}>Spend Money</button>
+				<button onClick={props.giveMoney}>Give me money</button>
+			</div>
+			<AddItem />
+		</div>
+	);
 };
 
 export default Student;
